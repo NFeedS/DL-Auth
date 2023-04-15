04a2f06546e16182f40a35b4b11ecff44688e0bb
Leonardo Malcotti <leonardo.malcotti@studenti.unitn.it>
2023-04-15 19:08:27 +0200

    converted repository to a crudrepository

--- src/main/java/com/nfeeds/data/auth/repository/ClientRepository.java
+++ src/main/java/com/nfeeds/data/auth/repository/ClientRepository.java
@@ -3,10 +3,9 @@ package com.nfeeds.data.auth.repository;
 import com.nfeeds.data.auth.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface ClientRepository extends JpaRepository<Client, String> {
+public interface ClientRepository extends CrudRepository<Client, String> {
 
-    @Query("select case when (count(u) > 0) then true else false end from Client u where u.clientId = :clientId")
-    Boolean checkClientIdExistence(@Param("clientId") String clientId);
 }
