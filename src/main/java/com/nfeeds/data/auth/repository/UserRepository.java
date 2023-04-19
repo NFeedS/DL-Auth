70d2231da99fa06efef22d6b8cf9edf56a0804f7
Leonardo Malcotti <leonardo.malcotti@studenti.unitn.it>
2023-04-19 14:55:50 +0200

    updated comment format

--- src/main/java/com/nfeeds/data/auth/repository/UserRepository.java
+++ src/main/java/com/nfeeds/data/auth/repository/UserRepository.java
@@ -4,15 +4,17 @@ import com.nfeeds.data.auth.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 /**
- * Interface used by Spring Data Rest to create CRUD endpoints over the User table.
- * The base endpoints generated will have the form:
- * GET|POST : {basePath}/users
- * GET|POST|PUT|DELETE : {basePath}/users/{id}
- * (plus the /profile and OPTIONS endpoint to get the endpoints metadata and options)
- *
- * Additional endpoint will be created based on the query functions of the interface:
- * GET : {basePath}/users/search/{specified_path|function_name}?{parameters}
- *
- * The response of the endpoint will follow HATEOAS specifications.
+ * <p> Interface used by Spring Data Rest to create CRUD endpoints over the User table. </p>
+ * <p> The base endpoints generated will have the form: </p>
+ * <code>
+ * <p><i> GET|POST : {basePath}/users </i></p>
+ * <p><i> GET|POST|PUT|DELETE : {basePath}/users/{id} </i></p>
+ * </code>
+ * <p>(plus the /profile and OPTIONS endpoint to get the endpoints metadata and options) </p>
+ * <p>
+ * <p> Additional endpoint will be created based on the query functions of the interface: </p>
+ * <code><p><i> GET : {basePath}/users/search/{specified_path|function_name}?{parameters} </i></p></code>
+ * <p>
+ * <p> The response of the endpoint will follow HATEOAS specifications. </p>
  */
 public interface UserRepository extends CrudRepository<User, String> {}
